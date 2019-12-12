5f1693cea2e7d8d2c5d33f79143713b5faf861b0
Racovtsi <racovtsiyosup12062000@gmail.com>
2019-12-12 22:41:25 +0200

    12.12.2019

---
+++ Unit1.h
@@ -0,0 +1,182 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+#include <Vcl.ExtCtrls.hpp>
+#include <Vcl.Imaging.jpeg.hpp>
+#include <Vcl.Dialogs.hpp>
+#include <Vcl.ExtDlgs.hpp>
+#include <Vcl.Buttons.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TImage *Image1;
+	TImage *Image2;
+	TImage *Image3;
+	TImage *Image4;
+	TImage *Image5;
+	TImage *Image6;
+	TImage *Image7;
+	TImage *Image8;
+	TImage *Image9;
+	TImage *Image10;
+	TLabel *Label1;
+	TImage *Image11;
+	TImage *Image12;
+	TImage *Image13;
+	TImage *Image14;
+	TImage *Image15;
+	TImage *Image16;
+	TImage *Image17;
+	TImage *Image18;
+	TImage *Image19;
+	TImage *Image20;
+	TImage *Image21;
+	TImage *Image22;
+	TImage *Image23;
+	TImage *Image24;
+	TImage *Image25;
+	TImage *Image26;
+	TImage *Image27;
+	TImage *Image28;
+	TImage *Image29;
+	TImage *Image30;
+	TImage *Image31;
+	TImage *Image32;
+	TImage *Image33;
+	TImage *Image34;
+	TImage *Image35;
+	TImage *Image36;
+	TImage *Image37;
+	TImage *Image38;
+	TImage *Image39;
+	TImage *Image40;
+	TImage *Image41;
+	TImage *Image42;
+	TImage *Image43;
+	TImage *Image44;
+	TImage *Image45;
+	TImage *Image46;
+	TImage *Image47;
+	TImage *Image48;
+	TImage *Image49;
+	TImage *Image50;
+	TPanel *Panel2;
+	void __fastcall Image1MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+		  int X, int Y);
+	void __fastcall OpenForma2(wchar_t uni[]);
+	void __fastcall Image2MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image3MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image4MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image5MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image6MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+		  int X, int Y);
+	void __fastcall Image7MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image8MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image9MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image10MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Button1Click(TObject *Sender);
+	void __fastcall Image11MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image12MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image13MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall n(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image15MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image16MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image17MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image18MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image19MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image20MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image21MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image22MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image23MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image24MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image25MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image26MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image27MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image28MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image29MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image30MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image31MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image32MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image33MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image34MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image35MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image36MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image37MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image38MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image39MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image40MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image41MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image42MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image43MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image44MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image45MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image46MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image47MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image48MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image49MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Image50MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall FormCreate(TObject *Sender);
+
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
---
+++ Unit2.cpp
@@ -0,0 +1,363 @@
+﻿//---------------------------------------------------------------------------
+#include <vcl.h>
+#include <vector>
+#pragma hdrstop
+
+#include "Unit1.h"
+#include "Unit2.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TForm2 *Form2;
+
+  TComponentList *DImageList; //список дин. имейджов
+  AnsiString spinName;
+  TImage* img;
+  int i = 0, left = -10, top = 320, temp = 0, temp1 = 0;
+  const int AR[9] = {1,1,1,1,1,1,1,1,1};
+  const int KR[18] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+  bool ar = false;
+  bool kr = false;
+  bool view = true;
+
+
+  std::vector<int>winSpin;
+  std::vector<int>trueSpin;
+
+ //функція створює на формі зображення вибраного спіна(1,2,3)
+void __fastcall TForm2::Spin(int spin){
+
+ switch(spin){
+  case 1 :
+  {
+   spinName = "updown";
+   if(!(i == 16))
+	trueSpin.push_back(1);
+  }
+  break;
+  case 2 :
+  {
+   spinName = "up";
+   if(!(i == 16))
+	trueSpin.push_back(2);
+  }
+  break;
+  case 3 :
+  {
+   spinName = "down";
+   if(!(i == 16))
+	trueSpin.push_back(3);
+  }
+  break;
+  case 4 :  //Добавление Ar
+  {
+  if(i == 0)
+   {
+	spinName = "ar";
+	ar = true;
+	for(int j = 0; j<9; j++)
+	trueSpin.push_back(AR[j]);
+   }
+  else
+   {
+  ShowMessage("\"Ar\" можна додати тільки на початку");
+  view = false;
+   }
+  }
+  break;
+  case 5 : //Добавление Kr
+  {
+  if(i == 0)
+   {
+	spinName = "kr";
+	kr = true;
+	for(int j = 0; j<18; j++)
+	trueSpin.push_back(KR[j]);
+   }
+  else
+   {
+  ShowMessage("\"Kr\" можна додати тільки на початку");
+  view = false;
+   }
+  }
+  break;
+  case 6 :  //Удаление имейдж компонента
+  {
+  if(ar == true && i == 1)
+   {
+	for(int j = 0; j<9; j++)
+	 trueSpin.pop_back();
+	 ar = false;
+   }
+  else if(kr == true && i == 1)
+   {
+	for(int j = 0; j<18; j++)
+	 trueSpin.pop_back();
+	 kr = false;
+   }
+  else
+   trueSpin.pop_back();
+   view = false;
+
+   if(!DImageList->Count==0)
+   {
+	DImageList->Delete(DImageList->Count-1);
+	i--;
+
+
+   if(ar == true || kr == true)
+	 {
+	  if(i == 1 || i == 2 || i == 7 || i == 10 || i == 11)
+	  top +=25;
+	 }
+   else
+	 {
+	  if(i == 1 || i == 2 || i == 5 || i == 6 || i == 9 || i == 10 || i == 15)
+	  top +=25;
+	 }
+   left -=35;
+   }
+  }
+  break;
+  case 7 :  //Проверка на правильно введенные спины
+  {
+   view = false;
+   if(trueSpin == winSpin) //winSpin берет из Form1
+   ShowMessage(L"    ВIРНО ツ");
+   else
+   ShowMessage(L"    невірно");
+  }
+  break;
+ }
+
+if(view == true) //Создание имейдж компонент
+ {
+  if(i == 16)
+   {
+   if(ar == true)
+	ShowMessage("Занадто багато спiнiв, замість цього використовуй \"Kr\" на початку");
+   else if(kr == true)
+	ShowMessage("Занадто багато спiнiв");
+   else
+	ShowMessage("Занадто багато спiнiв, замість цього використовуй \"Ar\" на початку");
+   }
+  else
+   {
+	if(ar == true || kr == true)
+	{
+	if(i == 1 || i == 2 || i == 7 || i == 10 || i == 11)
+	top -=25;
+	}
+	else
+	{
+	if(i == 1 || i == 2 || i == 5 || i == 6 || i == 9 || i == 10 || i == 15)
+	top -=25;
+	}
+   left +=35;
+
+   img = new TImage(Form2);
+   DImageList->Add(img);
+   img->Parent = Form2;
+   img->Left = left;
+   img->Top = top;
+   img->Width = Form2->ClientWidth - 10 - 20;
+   img->Visible = true;
+   img->Picture->LoadFromFile("Picture/spin/"+spinName+".bmp");
+   i++;
+   }
+ }
+else
+ {
+  view = true;
+ }
+}
+
+//---------------------------------------------------------------------------
+__fastcall TForm2::TForm2(TComponent* Owner)
+	: TForm(Owner)
+{
+
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm2::Button1Click(TObject *Sender)
+{ //Очищает и закрывает форму
+for(int j = 0; j<temp; j++)
+{
+ if(!DImageList->Count==0)
+ {
+	DImageList->Delete(DImageList->Count-1);
+	i--;
+	if(ar == true || kr == true)
+	 {
+	  if(i == 1 || i == 2 || i == 7 || i == 10 || i == 11)
+	  top +=25;
+	 }
+	else
+	 {
+	  if(i == 1 || i == 2 || i == 5 || i == 6 || i == 9 || i == 10 || i == 15)
+	  top +=25;
+	 }
+	left -=35;
+   }
+}
+trueSpin.clear();
+winSpin.clear();
+temp = 0;
+i = 0;
+Form2->Close();
+//Form2->ClientWidth = 675;
+Form1->Visible = true;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm2::FormClose(TObject *Sender, TCloseAction &Action)
+{ //Очищает и закрывает форму
+for(int j = 0; j<temp; j++)
+{
+ if(!DImageList->Count==0)
+ {
+  DImageList->Delete(DImageList->Count-1);
+  i--;
+  if(ar == true || kr == true)
+   {
+	if(i == 1 || i == 2 || i == 7 || i == 10 || i == 11)
+	top +=25;
+   }
+  else
+   {
+	if(i == 1 || i == 2 || i == 5 || i == 6 || i == 9 || i == 10 || i == 15)
+	top +=25;
+   }
+  left -=35;
+ }
+}
+trueSpin.clear();
+winSpin.clear();
+temp = 0;
+i = 0;
+//Form2->ClientWidth = 675;
+Form1->Visible = true;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm2::SpeedButton1Click(TObject *Sender)
+{
+Spin(1); //updown
+temp++;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm2::SpeedButton2Click(TObject *Sender)
+{
+Spin(2); //up
+temp++;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm2::SpeedButton3Click(TObject *Sender)
+{
+Spin(3); //down
+temp++;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm2::Button2Click(TObject *Sender)
+{
+Spin(6); //Очистить
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm2::FormCreate(TObject *Sender)
+{
+DImageList = new TComponentList; // создаем список
+DImageList->OwnsObjects = true;
+Application->Title = "";
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm2::Button3Click(TObject *Sender)
+{
+Spin(7); //Проверить
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm2::SpeedButton4Click(TObject *Sender)
+{
+Spin(4); //Ar
+temp++;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm2::FormShow(TObject *Sender)
+{
+if(Form2->Label1->Width < 735)
+ {
+  Panel3->Left = 130;
+  Panel4->Left = 257;
+  Panel5->Left = 441;
+  SpeedButton6->Left = 620;
+  Form2->ClientWidth = 680;
+ }
+else
+ {
+ switch(Form2->Label1->Width)
+  {
+  case 735 :
+   {
+	Panel3->Left = 150;
+	Panel4->Left = 277;
+	Panel5->Left = 461;
+	SpeedButton6->Left = 710;
+	Form2->ClientWidth = 770;
+   break;
+   }
+
+  case 823 :
+   {
+	Panel3->Left = 190;
+	Panel4->Left = 317;
+	Panel5->Left = 501;
+	SpeedButton6->Left = 798;
+	Form2->ClientWidth = 858;
+   break;
+   }
+
+  case 916 :
+   {
+	Panel3->Left = 230;
+	Panel4->Left = 357;
+	Panel5->Left = 541;
+	SpeedButton6->Left = 891;
+	Form2->ClientWidth = 951;
+   break;
+   }
+
+  case 940 :
+   {
+	Panel3->Left = 250;
+	Panel4->Left = 367;
+	Panel5->Left = 559;
+	SpeedButton6->Left = 915;
+	Form2->ClientWidth = 975;
+   break;
+   }
+
+  case 1033 :
+   {
+	Panel3->Left = 290;
+	Panel4->Left = 417;
+	Panel5->Left = 601;
+	SpeedButton6->Left = 1008;
+	Form2->ClientWidth = 1068;
+   break;
+   }
+  }
+ }
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm2::SpeedButton5Click(TObject *Sender)
+{
+Spin(5);
+temp++;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm2::SpeedButton6Click(TObject *Sender)
+{
+ShowMessage(L"Виконав Раковцi Йосип 41-К  ( ˇ෴ˇ )");
+}
+//---------------------------------------------------------------------------
+
+
